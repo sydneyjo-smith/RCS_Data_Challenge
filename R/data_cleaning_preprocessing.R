c9f903834da43a3483cf5ff2fe0e76c056415240
mattretford <matthewretford@gmail.com>
2025-02-02 17:06:52 +0000

    Data preprocessing step - new file

--- R/data_cleaning_preprocessing.R
+++ R/data_cleaning_preprocessing.R
@@ -4,13 +4,14 @@ library("readxl")
 library("dplyr")
 
 ### Load in data from excel file
-audit_meta_info <- read_excel("data/cancerdata_combined.xlsx", sheet = "audit_meta_info")
+audit_meta_info <- read_excel("data/raw_data/cancerdata_combined.xlsx", sheet = "audit_meta_info")
 indicators_trust <- read_excel("data/raw_data/cancerdata_combined.xlsx", sheet = "Indicators_trust")
-indicators_CA <- read_excel("data/cancerdata_combined.xlsx", sheet = "Indicators_CA")
-data_quality <- read_excel("data/cancerdata_combined.xlsx", sheet = "data_quality")
-metric_info <- read_excel("data/cancerdata_combined.xlsx", sheet = "metric_info")
+indicators_CA <- read_excel("data/raw_data/cancerdata_combined.xlsx", sheet = "Indicators_CA")
+data_quality <- read_excel("data/raw_data/cancerdata_combined.xlsx", sheet = "data_quality")
+metric_info <- read_excel("data/raw_data/cancerdata_combined.xlsx", sheet = "metric_info")
 
-
+data_quality_targets <- read.csv("data/raw_data/data_quality_targets.csv")
+performance_indicator_metric_type <- read.csv("data/raw_data/performance_indicator_metric_type.csv")
 
 ################## indicators_trust
 
@@ -81,10 +82,30 @@ indicators_trust <- indicators_trust %>%
 # Values 0-100 (NKCA, NOCA, NOGCA, NPaCA)
 # Mixed values (NBOCA (unadjusted-30/90 are 0-1, others 0-100))
 
+indicators_trust <- indicators_trust %>%
+  mutate(
+    pact = if_else(pact > 1, pact/100, pact),
+    mav = if_else(mav > 1, mav/100, mav),
+    mav_ca = if_else(mav_ca > 1, mav_ca/100, mav_ca),
+    mav_nat = if_else(mav_nat > 1, mav_nat/100, mav_nat))
+
+
 # Join the directional variable
+indicators_trust <- indicators_trust %>%
+  left_join(performance_indicator_metric_type, by=c("audit", "metric_name"))
+
+# Add cancer audit name
+audit_meta_info <- audit_meta_info %>%
+  rename(audit = audit_name_acronym)
+
+indicators_trust <- indicators_trust %>%
+  left_join(audit_meta_info, by=c("audit"))
 
 
 # denominator "0" (NNHLA)
 # demoninator "1","2","3","4" (NOCA - Clatterbridge Cancer Centre NHS Foundation Trust. Data should be supressed for under 5)
 
+### Saving processed files
+
+write.csv(indicators_trust, "data/processed_data/indicators_trust.csv", row.names = FALSE,na = "")
 
