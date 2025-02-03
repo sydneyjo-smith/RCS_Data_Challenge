a3681c3b9958f02eb360660be86439ceb1dd55a3
mattretford <matthewretford@gmail.com>
2025-02-03 06:55:41 +0000

    Dashboard

--- R/data_analysis_plots.R
+++ R/data_analysis_plots.R
@@ -8,20 +8,23 @@ library(plotly)
 library(tidyr)
 library(grid)
 
-################## Load in pre-processed data ##################
+########################## Load pre-processed data #############################
+
 indicators_trust <- read_csv("data/processed_data/indicators_trust.csv")
 data_quality <- read_csv("data/processed_data/data_quality.csv")
 
 
-################## PLOT 1  ##################
+#################################### PLOT 1  ####################################
 
 
 
-################## PLOT 2  ##################
+#################################### PLOT 2  ####################################
 
 
 
-################## PLOT 3  ##################
+#################################### PLOT 3  ####################################
+
+############ Data preparation ############
 
 plot_2_data <- indicators_trust
 
@@ -32,27 +35,18 @@ unique_quarters <- plot_2_data %>%
 
 quarter_levels <- unique_quarters$quarter_year
 
-
 plot_2_data$quarter_year <- factor(plot_2_data$quarter_year,
                             levels = quarter_levels,
                             ordered = TRUE)
 
 
-plot_2_data_filtered <- plot_2_data %>%
-  filter(audit == 'NNHLA' &
-           trust_code == 'RJ1')
-
-# filtered_data <- data %>%
-#   filter(trust_code == 'RJ1')
-
-
 # Reshape the data to long format
-data_long <- plot_2_data_filtered %>%
+plot_2_data_long <- plot_2_data %>%
   pivot_longer(cols = c(mav, mav_ca, mav_nat),
                names_to = "category",
                values_to = "percentage")
 
-data_long = data_long %>%
+plot_2_data_long <- plot_2_data_long %>%
   mutate(
     den_label = if_else(is.na(denominator),"Suppressed", as.character(denominator)),
     text = paste(
@@ -63,21 +57,33 @@ data_long = data_long %>%
   )
 
 # Convert to factor with custom labels
-data_long <- data_long %>%
+plot_2_data_long <- plot_2_data_long %>%
   mutate(category = factor(category,
                            levels = c("mav", "mav_ca", "mav_nat"),
                            labels = c("Trust moving average",
                                       "Cancer alliance moving average",
                                       "National moving average")))
+
+############ Save prepared data ############
+
+write.csv(plot_2_data_long, "data/processed_data/data_long.csv", row.names = FALSE,na = "")
+
+
+############ Plotting ############
+
+plot_2_data_filtered <- plot_2_data_long %>%
+  filter(audit == 'NNHLA' &
+           trust_code == 'RJ1')
+
 # Line plot with ggplot2
-performance_plot <- ggplot(data_long, aes(x = quarter_year, y = percentage, color = category, group = category, text=text)) +
-  geom_line(data = filter(data_long, category == "Trust moving average"), size = 1, ) +  # Lines for ca_mav and na_mav
-  geom_line(data = filter(data_long, category != "Trust moving average"), size = 1, linetype="dotted") +  # Lines for ca_mav and na_mav
-  geom_point(data = filter(data_long, category == "Trust moving average"),
+performance_plot <- ggplot(plot_2_data_filtered, aes(x = quarter_year, y = percentage, color = category, group = category, text=text)) +
+  geom_line(data = filter(plot_2_data_filtered, category == "Trust moving average"), size = 1, ) +  # Lines for ca_mav and na_mav
+  geom_line(data = filter(plot_2_data_filtered, category != "Trust moving average"), size = 1, linetype="dotted") +  # Lines for ca_mav and na_mav
+  geom_point(data = filter(plot_2_data_filtered, category == "Trust moving average"),
              aes(size = denominator),
              alpha = 0.8) +  # Points for mav, size based on denominator
   scale_size_continuous(range = c(5, 10)) +  # Adjusts point size scale
-  geom_point(data = filter(data_long, category == "Trust moving average" & is.na(denominator)),
+  geom_point(data = filter(plot_2_data_filtered, category == "Trust moving average" & is.na(denominator)),
              shape = 21,  # Shape 21 allows color & fill customization
              fill = "white",
              color = "red",
@@ -101,3 +107,4 @@ interactive_line_plot <- interactive_line_plot %>%
 interactive_line_plot
 
 
+#################################### PLOT 4  ####################################
